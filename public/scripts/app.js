8d0849099e6baa458fe7aed5d7ef22a5202beb50
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-03-03 13:50:40 +0100

    using babel

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,11 @@
 console.log("App.js is running");
 
 // JSX - Javascript XML
+var template = React.createElement(
+	'h1',
+	{id: "someid"},
+	'Something new form app.js'
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
