b2502f024c10280a6aa9b1fdb013a00e25524101
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-03-04 17:58:20 +0100

    random task

--- src/app.js
+++ src/app.js
@@ -23,6 +23,13 @@ const onRemoveAll = () => {
 	render();
 };
 
+const onMakeDecision = () => {
+	const randomNum = Math.floor(Math.random() * app.options.length);
+	const option = app.options[randomNum];
+	alert(option);
+};
+
+
 const appRoot = document.getElementById('app');
 
 
@@ -32,7 +39,7 @@ const render = () => {
 			<h1>{app.title}</h1>
 			{app.subtitle && <p>{app.subtitle}</p>}
 			<p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-			<p>{app.options.length}</p>
+			<button disabled={app.options.length === 0} onClick={onMakeDecision} className="btn btn-lg btn-primary">What should I do?</button>
 			<button onClick={onRemoveAll} className="btn btn-danger">Remove All</button>
 			<ol>
 				{/* map over app.options getting back an array of list (set key and text) */}
